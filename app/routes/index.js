09686635e2a8804e9d5a5daaeaf5dd96503de315
Matheus Pimentel <matheus200713@gmail.com>
2018-07-31 08:32:05 -0400

    implementando a validação do login

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,6 @@ module.exports = function(application){
     });
 
     application.post('/autenticar', function(req, res){
-        // application.app.controllers.index.index(application, req, res)
+        application.app.controllers.index.autenticar(application, req, res)
     });
 };
